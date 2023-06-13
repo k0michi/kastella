f368d3b63348e1593e1898c2c27d60cc6052e2c8
Komichi <k0michi@koyomi.co>
2023-06-13 10:45:49 +0900

    Fix

--- src/renderer/components/node-content/quote-node-content.tsx
+++ src/renderer/components/node-content/quote-node-content.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { HeadingNode, MathNode, QuoteNode, TextNode } from '../../node';
+import { QuoteNode } from '../../node';
 import { inlineNodeToElement } from '../../tree';
 import { useModel } from 'kyoka';
 import Model from '../../model';
@@ -9,10 +9,5 @@ export interface QuoteNodeContentProps {
 }
 
 export default function QuoteNodeContent(props: QuoteNodeContentProps) {
-  const model = useModel<Model>();
-  const headingDepth = model.library.getHeadingDepth(props.node);
-  const COEFF = 2 ** (1 / 6);
-  const fontSize = Math.max(2 / COEFF ** headingDepth, 1);
-
   return <div className='content quote-node'>{inlineNodeToElement(props.node.content)}</div>;
 }
