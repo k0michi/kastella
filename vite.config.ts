9725b27f2adfe9ab2ed3d55c958dd0a8419855a7
Komichi <k0michi@koyomi.co>
2023-06-24 22:15:26 +0900

    Use rollup-plugin-copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +8,23 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist/renderer',
-    target: 'es2021'
+    target: 'es2021',
+    rollupOptions: {
+      plugins: [
+        copy({
+          targets: [
+            {
+              src: 'node_modules/@excalidraw/excalidraw/dist/excalidraw-assets',
+              dest: 'dist/excalidraw-assets'
+            },
+            {
+              src: 'node_modules/@excalidraw/excalidraw/dist/excalidraw-assets-dev',
+              dest: 'dist/excalidraw-assets-dev'
+            }
+          ]
+        })
+      ]
+    }
   },
   publicDir: 'dist'
 });
