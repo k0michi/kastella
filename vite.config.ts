2060bf3e353b2171b444b6963b5d074601656e77
Komichi <k0michi@koyomi.co>
2023-06-24 22:26:55 +0900

    Revert "Use rollup-plugin-copy"
    This reverts commit 9725b27f2adfe9ab2ed3d55c958dd0a8419855a7.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import copy from 'rollup-plugin-copy';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,23 +7,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist/renderer',
-    target: 'es2021',
-    rollupOptions: {
-      plugins: [
-        copy({
-          targets: [
-            {
-              src: 'node_modules/@excalidraw/excalidraw/dist/excalidraw-assets',
-              dest: 'dist/excalidraw-assets'
-            },
-            {
-              src: 'node_modules/@excalidraw/excalidraw/dist/excalidraw-assets-dev',
-              dest: 'dist/excalidraw-assets-dev'
-            }
-          ]
-        })
-      ]
-    }
+    target: 'es2021'
   },
   publicDir: 'dist'
 });
