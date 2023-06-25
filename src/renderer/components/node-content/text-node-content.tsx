568e2f3d0b7c50de0e70f02d2fa468888cea8c69
Komichi <k0michi@koyomi.co>
2023-06-25 13:20:53 +0900

    Fix anchor color

--- src/renderer/components/node-content/text-node-content.tsx
+++ src/renderer/components/node-content/text-node-content.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import { TextNode } from '../../node';
 import { inlineNodeToElement } from '../../tree';
+import styled from 'styled-components';
+
+const DivTextNode = styled.div`
+  a {
+    color: inherit;
+  }
+`;
 
 export interface TextNodeContentProps {
   node: TextNode;
 }
 
 export default function TextNodeContent(props: TextNodeContentProps) {
-  return <div className='content text-node'>{inlineNodeToElement(props.node.content)}</div>;
+  return <DivTextNode>{inlineNodeToElement(props.node.content)}</DivTextNode>;
 }
