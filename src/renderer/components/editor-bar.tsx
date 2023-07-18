0fd56e917f9273e3467f406400a7ccb2f3c6a303
Komichi <k0michi@koyomi.co>
2023-07-18 14:44:56 +0900

    Make editor bar unselectable

--- src/renderer/components/editor-bar.tsx
+++ src/renderer/components/editor-bar.tsx
@@ -37,6 +37,10 @@ const DivEditorBarSection = styled.div`
   }
 `;
 
+const DivUnselectable = styled.div`
+  user-select: none;
+`;
+
 export default function EditorBar() {
   const model = useModel<Model>();
   const writeOnly = useObservable(model.writeOnly);
@@ -47,35 +51,37 @@ export default function EditorBar() {
 
   return <div id='editor-bar'>
     <DivEditorBarSection>
-      {
-        view?.type == ViewType.Directory ?
-          `${model.library.getPath((view as DirectoryView).parentID!)}`
-          : view?.type == ViewType.Tag ?
-            `#${model.library.getTag((view as TagView).tag)?.name}`
-            : view?.type == ViewType.Date ?
-              `@${(view as DateView).date}`
-              : null
-      }
+      <DivUnselectable>
+        {
+          view?.type == ViewType.Directory ?
+            `${model.library.getPath((view as DirectoryView).parentID!)}`
+            : view?.type == ViewType.Tag ?
+              `#${model.library.getTag((view as TagView).tag)?.name}`
+              : view?.type == ViewType.Date ?
+                `@${(view as DateView).date}`
+                : null
+        }
+      </DivUnselectable>
     </DivEditorBarSection>
     <DivEditorBarSection>
-      <div className='checkbox'>
+      <DivUnselectable>
         <input checked={writeOnly} type="checkbox" id="write-only" onChange={e => {
           model.setWriteOnly(e.target.checked);
         }} />
         <label htmlFor="write-only">Write-only</label>
-      </div>
-      <div className='checkbox'>
+      </DivUnselectable>
+      <DivUnselectable>
         <input checked={lineNumberVisibility} type="checkbox" id="line-number-visibility" onChange={e => {
           model.setLineNumberVisibility(e.target.checked);
         }} />
         <label htmlFor="line-number">Line number</label>
-      </div>
-      <div className='checkbox'>
+      </DivUnselectable>
+      <DivUnselectable>
         <input checked={dateVisibility} type="checkbox" id="date-visibility" onChange={e => {
           model.setDateVisibility(e.target.checked);
         }} />
         <label htmlFor="date-visibility">Date</label>
-      </div>
+      </DivUnselectable>
       <TextAreaSearch rows={1} onChange={e => model.setSearch(e.target.value)} value={search} />
     </DivEditorBarSection>
   </div>;
